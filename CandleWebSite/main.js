7428897b2419a17e851d2880b03f372aec900154
Ryan Smith <51637165+mrrjs@users.noreply.github.com>
2020-07-08 11:41:03 +0100

    Update main.js

--- CandleWebSite/main.js
+++ CandleWebSite/main.js
@@ -1,16 +1,36 @@
+/*On scroll fade in/out*/
 $(window).on("load",function() {
   $(window).scroll(function() {
     var windowBottom = $(this).scrollTop() + $(this).innerHeight();
     $(".fade").each(function() {
-      /* Check the location of each desired element */
       var objectBottom = $(this).offset().top + $(this).outerHeight();
       
-      /* If the element is completely within bounds of the window, fade it in */
-      if (objectBottom < windowBottom) { //object comes into view (scrolling down)
+      if (objectBottom < windowBottom) { 
         if ($(this).css("opacity")==0) {$(this).fadeTo(500,1);}
-      } else { //object goes out of view (scrolling up)
+      } else { 
         if ($(this).css("opacity")==1) {$(this).fadeTo(500,0);}
       }
     });
-  }).scroll(); //invoke scroll-handler on page-load
+  }).scroll();
 });
+
+/*Cookies*/
+function PopUp(hideOrshow) {
+    if (hideOrshow == 'hide') {
+        document.getElementById('ac-wrapper').style.display = "none";
+    }
+    else  if(localStorage.getItem("popupWasShown") == null) {
+        localStorage.setItem("popupWasShown",1);
+        document.getElementById('ac-wrapper').removeAttribute('style');
+    }
+}
+window.onload = function () {
+    setTimeout(function () {
+        PopUp('show');
+    }, 0);
+}
+
+
+function hideNow(e) {
+    if (e.target.id == 'ac-wrapper') document.getElementById('ac-wrapper').style.display = 'none';
+}
